8427c091d46ac724369bcea9c20d8e4e83288f4b
Nami W <1160688+namiwang@users.noreply.github.com>
2025-01-21 15:46:42 +0000

    Draft (#4)
    boot: first draft

---
+++ assets/main.js
@@ -0,0 +1,106 @@
+const TOOL_DISCUSSION_IDS = {
+  primer: 2,
+  polaris: 3,
+  govuk: 5,
+  csszero: 6,
+  railsdesigner: 7,
+  railsui: 8,
+  rubyui: 9,
+  zestui: 10,
+  protos: 11,
+  shadcnonrails: 12,
+}
+
+const EMOJI_MAP = {
+  THUMBS_UP: '👍',
+  THUMBS_DOWN: '👎',
+  LAUGH: '😂',
+  HOORAY: '🎉',
+  CONFUSED: '😕',
+  HEART: '❤️',
+  ROCKET: '🚀',
+  EYES: '👀',
+}
+
+function toolReactions(tool) {
+  return {
+    commentsCount: 0,
+    reactions: {},
+    isLoading: true,
+    async fetchData() {
+      try {
+        if (!tool) return
+
+        const discussionId = TOOL_DISCUSSION_IDS[tool]
+        if (!discussionId) return
+
+        const url = `https://giscus.app/api/discussions?repo=namiwang%2Frails-style&number=${discussionId}&strict=true&last=15`
+        const response = await fetch(url)
+        const data = await response.json()
+
+        this.commentsCount = data.discussion.totalCommentCount
+
+        // sample
+        // {
+        //   "THUMBS_UP": {
+        //       "count": 565,
+        //       "viewerHasReacted": false
+        //   }
+        //   ...
+        // }
+        this.reactions = data.discussion.reactions
+
+        this.isLoading = false
+      } catch (error) {
+        // console.error(`Error fetching discussion`, error)
+      }
+    },
+    init() {
+      this.fetchData()
+    },
+    getEmoji(emoji) {
+      return EMOJI_MAP[emoji]
+    },
+  }
+}
+
+function commentsPanel() {
+  return {
+    isPanelOpen: false,
+    selectedTool: null,
+    openCommentsPanel(tool) {
+      this.isPanelOpen = true
+      this.selectedTool = tool
+
+      this.$nextTick(() => {
+        const script = document.createElement('script')
+        script.src = "https://giscus.app/client.js"
+        script.dataset.repo = "namiwang/rails-style"
+        script.dataset.repoId = "R_kgDONjz4uw"
+        script.dataset.mapping = "number"
+        script.dataset.term = TOOL_DISCUSSION_IDS[tool]
+        script.dataset.reactionsEnabled = "1"
+        script.dataset.emitMetadata = "0"
+        script.dataset.inputPosition = "top"
+        script.dataset.theme = "light"
+        script.dataset.lang = "en"
+        // script.dataset.loading = "lazy"
+        script.crossorigin = "anonymous"
+        script.async = true
+
+        // script.onload = () => {
+        //   console.log('Remote script loaded and executed')
+        // }
+        // script.onerror = () => {
+        //   console.error('Failed to load the remote script')
+        // }
+
+        document.querySelector('#comments-panel').appendChild(script)
+      })
+    },
+    closeCommentsPanel() {
+      this.isPanelOpen = false
+      this.selectedTool = null
+    },
+  }
+}
---
+++ index.md
@@ -0,0 +1,45 @@
+---
+layout: home
+---
+
+<style>
+  {% include assets/main.css %}
+</style>
+
+_rails.style_ is a curated resource hub for Rails developers. It currently focuses on UI related libraires, and will expand to cover other topics (background jobs, administration, mailers, newsletters, etc.) in the future.
+
+## Rendering Solutions
+
+Early Rails applications use ERB (or others like Haml) partials and bloated helpers. Modern Rails projects often emphasizes encapsulation and component-based design. There's a few libraries to create reusable and testable UI components.
+
+|                                                            | Encapsulation | **Templating**        | **Output**          | **Performance**<sup>*</sup> |
+| ---------------------------------------------------------- | ------------- | --------------------- | ------------------- | --------------------------- |
+| ERB Partial                                                | Bad           | Template              | HTML                | 3                           |
+| [ViewComponent](https://viewcomponent.org/)                | Not Bad       | Ruby Class + Template | HTML                | 6                           |
+| [Cells](https://github.com/trailblazer/cells)              | Not Bad       | Ruby Class + Template | HTML                | 2                           |
+| [Hanami::View](https://github.com/hanami/view)             | Not Bad       | Ruby Class + Template | HTML, JSON, XML     | -                           |
+| [Phlex](https://www.phlex.fun/)                            | Not Bad       | Pure Ruby             | HTML (more planned) | 8                           |
+| [Papercraft](https://github.com/digital-fabric/papercraft) | Not Bad       | Pure Ruby             | HTML, JSON, XML     | 9                           |
+
+\* performance data source: [view-layer-benchmarks](https://github.com/KonnorRogers/view-layer-benchmarks)
+
+## Rails-specific Design Systems & UI Libraries
+
+{% include rails_ui.html %}
+
+## Posts and Discussions
+
+<ul>
+{% for post in site.data.posts %}
+  <li>
+    {% include link_preview.html url=post %}
+  </li>
+{% endfor %}
+</ul>
+
+<script defer src="/assets/main.js"></script>
+<script defer src="https://unpkg.com/alpinejs-component@latest/dist/component.min.js"></script>
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.8/dist/cdn.min.js"></script>
+
+<!-- TODO comments panel performance (cache?) -->
+<!-- TODO git activity sparkline -->
